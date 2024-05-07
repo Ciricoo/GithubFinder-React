410b6153367cc0fd97b6607fa1773de0aca55b48
Estagiario 3 <estagiario3@movtech.com.br>
2024-05-07 15:26:49 -0300

    creating error component

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -2,13 +2,22 @@ import { UserProps } from "../types/user";
 import Search from "../components/Search";
 import { useState } from "react";
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async(userName: string) =>{
+        setError(false)
+        setUser(null)
         const res = await fetch(`https://api.github.com/users/${userName}`);
         const data = await res.json();
+
+        if(res.status === 404){
+            setError(true);
+            return
+        }
 
         const {avatar_url, login, location, followers, following} = data;
 
@@ -25,6 +34,7 @@ const Home = () => {
     return <div>
         <Search loadUser={loadUser} />
         {user && <p><User {...user} /></p>}
+        {error && <Error />}
     </div>
 }
 
